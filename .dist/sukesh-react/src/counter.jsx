12ae591c3018006ee6eb2fc672f149abd9ecbaf7
Sukesh R <sukesh2007raja@gmail.com>
2026-06-02 22:12:27 +0530

    E commerse

---
+++ .dist/sukesh-react/src/counter.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+const counter     =()=>{
+ } 
+ const handleincrement =()=>{
+ 
+    setdata((prev)=prev+1)
+ }
+ const handleDecrement =()=>{
+    Setdata((prev)=prev-1)
+ }
+ function handlereset=()=>{
+    setdata((prev)=prev=0)
+
+ }
+
+       
+    
+export default counter
